6b9a221f8f97fc090ce3ed746b0203d942ac0bce
psatasiya <psatasiya@boruapps.com>
2024-12-24 10:47:56 +0530

    add task form

--- src/Controller/Panel/TaskController.php
+++ src/Controller/Panel/TaskController.php
@@ -3,7 +3,10 @@
 namespace App\Controller\Panel;
 
 use App\Entity\Task;
+use App\Form\TaskType;
 use DateTime;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Omines\DataTablesBundle\Column\DateTimeColumn;
 use Omines\DataTablesBundle\Column\TextColumn;
 use Omines\DataTablesBundle\DataTableFactory;
@@ -18,6 +21,7 @@ class TaskController extends AbstractController
 {
     private $factory;
 
+
     public function __construct(DataTableFactory $factory)
     {
         $this->factory = $factory;
@@ -27,10 +31,10 @@ class TaskController extends AbstractController
     public function index(Request $request, DataTableFactory $dataTableFactory): Response
     {
         $table = $this->factory->create()
-            ->add('title', TextColumn::class,["label" => "Title", "searchable" => true])
-            ->add('dueDate', DateTimeColumn::class,["label" => "Due Date","format" => "d-m-Y"])
-            ->add('status', TextColumn::class,["label" => "Status",'field' => 'status.name'])
-            ->add('category', TextColumn::class,["label" => "Category",'field' => 'category.name'])
+            ->add('title', TextColumn::class, ["label" => "Title", "searchable" => true])
+            ->add('dueDate', DateTimeColumn::class, ["label" => "Due Date", "format" => "d-m-Y"])
+            ->add('status', TextColumn::class, ["label" => "Status", 'field' => 'status.name'])
+            ->add('category', TextColumn::class, ["label" => "Category", 'field' => 'category.name'])
             ->createAdapter(ORMAdapter::class, [
                 'entity' => Task::class,
             ])->handleRequest($request);
@@ -39,5 +43,32 @@ class TaskController extends AbstractController
         }
 
         return $this->render('task/list.html.twig', ['datatable' => $table]);
+    }
+
+
+    #[Route('/task/add', name: 'app_task_add')]
+    public function add(Request $request, EntityManagerInterface $entityManager)
+    {
+        $task = new Task();
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $task->setUser($this->getUser());
+            $entityManager->persist($task);
+            $entityManager->flush();
+
+            $this->addFlash(
+                'success',
+                'Task added successfully.'
+            );
+
+            return $this->redirectToRoute('app_task');
+        }
+
+        return $this->render('task/add.html.twig', [
+            'task' => $task,
+            'form' => $form,
+        ]);
     }
 }
